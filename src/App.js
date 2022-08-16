c252772be9534d3be02ab72ba8221661d49137be
ghaniz <ghanizahid@hotmail.com>
2022-08-16 00:35:10 -0400

    Day 8 mocking Network activity

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
-
 function App() {
-  return (
+const [gitHubName, setGitHubName] = useState('') 
+const [GitHubAvatar, setGitHubAvatar] = useState('')
+const [GitHubURL, setGitHubURL] = useState('')                    
+
+
+useEffect(() => {
+    fetch('https://api.github.com/users/gzahid')
+    .then(res => res.json())
+    .then(data => {
+        setGitHubName(data.name)
+        setGitHubAvatar(data.avatar_url)
+        setGitHubURL(data.html_url)
+    })
+}, [])
+
+return (
     <div className="App">
-      <h1>Github Profile Info:</h1>
+        <h1>Github Profile Info:</h1>
+        <h2>{gitHubName}</h2>
+        <p>
+          <a href={GitHubURL} target="_blank">
+            <button>Link to Github Profile</button>
+          </a>
+        </p>
+        <p>
+          <img src={GitHubAvatar} alt="" height='' width='å' />
+          
+        </p>
+
     </div>
-  );
+);
 }
 
-export default App;
+export default App
+    
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,33 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
+require('jest-fetch-mock').enableMocks()
 
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+  })
 
+describe('GitHub REST API using jest fetch mock', () =>{
 
+    test("receives GitHub name", async () => {
+        fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+        render(<App />)
+        const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+        expect(gitHubName).toHaveTextContent('coder')
+    })
+
+    test("receives GitHub URL", async () => {
+        fetch.mockResponseOnce(JSON.stringify({html_url: 'https://github.com/gzahid'}))
+        render(<App />)
+        const GitHubURL = await waitFor(() => screen.getByRole('link'))
+        expect(GitHubURL).toHaveAttribute('href', expect.stringContaining('gzahid'))
+    })
+
+    test("receives GitHub Avatar URL", async () => {
+        fetch.mockResponseOnce(JSON.stringify({avatar_url: 'https://avatars.githubusercontent.com/u/8547997?v=4'}))
+        render(<App />)
+        const GitHubAvatar = await waitFor(() => screen.getByRole('img'))
+        expect(GitHubAvatar).toHaveAttribute('src', expect.stringContaining('githubusercontent'))
+    })
+
+})
